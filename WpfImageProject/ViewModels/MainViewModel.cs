32992426117862c69b38497fa10309cba5ec65e2
Andrei Chirun <djanri>
2024-07-25 15:11:54 +0300

    FEATURE: added second example with loading image without its locking

--- WpfImageProject/ViewModels/MainViewModel.cs
+++ WpfImageProject/ViewModels/MainViewModel.cs
@@ -2,6 +2,7 @@
 using WpfImageNet8.Infrastructure;
 using System.IO;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace WpfImageNet8.ViewModels
 {
@@ -14,6 +15,10 @@ namespace WpfImageNet8.ViewModels
 		
         private string _sourceImageFile = Path.Combine(SourceFolderImagePath, $"{ImageName}.{ImageExt}");
         private string _imagePath;
+        private string _imageFileName;
+        private string _imageSecondFileName;
+        private BitmapImage _imageSecond;
+
         private ICommand _loadImageCommand;
         private ICommand _deleteImageCommand;
 
@@ -27,6 +32,36 @@ namespace WpfImageNet8.ViewModels
             set 
             { 
                 _imagePath = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string ImageFileName
+        {
+            get => _imageFileName;
+            set 
+            { 
+                _imageFileName = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public string ImageSecondFileName
+        {
+            get => _imageSecondFileName;
+            set 
+            { 
+                _imageSecondFileName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BitmapImage ImageSecond
+        {
+            get => _imageSecond;
+            set 
+            {
+                _imageSecond = value;
                 OnPropertyChanged();
             }
         }
@@ -56,6 +91,17 @@ namespace WpfImageNet8.ViewModels
                         {
                             case "1":
                                 ImagePath = Path.GetFullPath(destPath);
+                                ImageFileName = Path.GetFileName(destPath);
+                                break;
+                            case "2":
+                                BitmapImage bitmapImage = new BitmapImage();
+                                bitmapImage.BeginInit();
+                                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                                bitmapImage.UriSource = new Uri(Path.GetFullPath(destPath));
+                                bitmapImage.EndInit();
+                                bitmapImage.Freeze();
+                                ImageSecond = bitmapImage;
+                                ImageSecondFileName = Path.GetFileName(destPath);
                                 break;
                         }
                     }
@@ -81,6 +127,12 @@ namespace WpfImageNet8.ViewModels
                                 {
                                     case "1":
                                         ImagePath = string.Empty;
+                                        ImageFileName = string.Empty;
+                                        MessageBox.Show("Image is deleted successfully");
+                                        break;
+                                    case "2":
+                                        ImageSecond = null;
+                                        ImageSecondFileName = string.Empty;
                                         MessageBox.Show("Image is deleted successfully");
                                         break;
                                 }
